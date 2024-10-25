4a1a08f45ddfc7e7881dd3d48e95b50fc1c112f2
notkooru <notkooru@gmail.com>
2024-10-25 04:28:25 -0600

    Created CRUD section

--- main.py
+++ main.py
@@ -3,3 +3,16 @@ import os
 def paste_to_clipboard(credentials):
     command = "echo | set /p null=" + credentials.strip() + "| clip"
     os.system(command)
+
+# CRUD
+def account_create():
+    pass
+
+def account_read():
+    pass
+
+def account_update():
+    pass
+
+def account_delete():
+    pass
